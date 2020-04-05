738f0b9653db0c67d49a986818ca7d0c2871c479
Mélanie <marquesmelanie5@gmail.com>
2020-04-05 23:30:47 +0200

    constants

--- src_project/Lab_Kafka/src/main/scala/Drone1/ProducerDrone1.scala
+++ src_project/Lab_Kafka/src/main/scala/Drone1/ProducerDrone1.scala
@@ -3,6 +3,7 @@ package Drone1
 import java.text.{DateFormat, SimpleDateFormat}
 import java.util.{Calendar, Date, Properties}
 
+import Utils.Constants
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
 object ProducerDrone1 extends App {
@@ -14,46 +15,18 @@ object ProducerDrone1 extends App {
   props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
   props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
-  // bornes random id
-  val start = 1
-  val end   = 10
-
-  // bornes coordinates
-  val startC = 1
-  val endC = 100
-
-  //bornes cases :
-  val startP = 1
-  val endP = 3
-
   //drone ID
   val id = 1
 
-  //Picture :
-  val picture = "http://vaniquertonpanda.com"
-
-  // Alert message possibilities
-  val possibleAlerts = Seq(
-    "666 - Pandora a mangé le drone",
-    "001 - Ben fait du Biryani sur la voie publique",
-    "002 - Anil tente d'assassiner Brenda",
-    "003 - Louis a laissé ses bouteilles de vodka dans la rue",
-    "004 - Mélanie a mit un air KO à CHEREL",
-    "005 - Sébastien deal des pizzas",
-    "006 - Kévin lance une révolution",
-    "007 - Thomas bricole le drone"
-  )
-
   // new producer
   val producer = new KafkaProducer[String, String](props)
-
 
 
   // Periodic notifications simulation
   while(true) {
 
     val rnd = new scala.util.Random
-    val caseD = startP +  rnd.nextInt( (endP - startP) + 1 )
+    val caseD = Constants.startP +  rnd.nextInt( (Constants.endP - Constants.startP) + 1 )
 
     //TODO
     // PREPARE DATA
@@ -64,8 +37,8 @@ object ProducerDrone1 extends App {
     val strDate: String = dateFormat.format(date)
 
     // Geographic coordinates
-    val x = startC + rnd.nextInt( (endC - startC) + 1 )
-    val y = startC + rnd.nextInt( (endC - startC) + 1 )
+    val x = Constants.startC + rnd.nextInt( (Constants.endC - Constants.startC) + 1 )
+    val y = Constants.startC + rnd.nextInt( (Constants.endC - Constants.startC) + 1 )
 
 
     //TODO
@@ -82,8 +55,8 @@ object ProducerDrone1 extends App {
     // ALERTS :
     //we generate a random alert
     else if (caseD == 2) {
-      val natureAlert = possibleAlerts(
-        rnd.nextInt(possibleAlerts.length)
+      val natureAlert = Constants.possibleAlerts(
+        rnd.nextInt(Constants.possibleAlerts.length)
       )
       producer.send(new ProducerRecord[String, String]("ALERT",
         "------ALERT------"
@@ -98,7 +71,7 @@ object ProducerDrone1 extends App {
     // SEND HELP :
     else {
       producer.send(new ProducerRecord[String, String]("SOS-SENDER", id.toString + "," + strDate +","+ x.toString +","+ y.toString
-      + "," + picture))
+      + "," + Constants.picture))
     }
 
     Thread.sleep(50000)
---
+++ src_project/Lab_Kafka/src/main/scala/Utils/Constants.scala
@@ -0,0 +1,30 @@
+package Utils
+
+object Constants {
+  // bornes random id
+  val start = 1
+  val end   = 10
+
+  // bornes coordinates
+  val startC = 1
+  val endC = 100
+
+  //bornes cases :
+  val startP = 1
+  val endP = 3
+
+  //Picture :
+  val picture = "http://vaniquertonpanda.com"
+
+  // Alert message possibilities
+  val possibleAlerts = Seq(
+    "666 - Pandora a mangé le drone",
+    "001 - Ben fait du Biryani sur la voie publique",
+    "002 - Anil tente d'assassiner Brenda",
+    "003 - Louis a laissé ses bouteilles de vodka dans la rue",
+    "004 - Mélanie a mit un air KO à CHEREL",
+    "005 - Sébastien deal des pizzas",
+    "006 - Kévin lance une révolution",
+    "007 - Thomas bricole le drone"
+  )
+}
