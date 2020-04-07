c92160ce147da59d8f566a3a3fec6f64e304382b
PrideTheDaemon <louis.prud_homme@efrei.net>
2020-04-07 18:05:42 +0200

    Removed target and .idea folders Added target and .idea folders in the gitignore Added SparkPOC

--- src_project/Lab_Kafka/build.sbt
+++ src_project/Lab_Kafka/build.sbt
@@ -2,6 +2,9 @@ name := "Lab_kafka"
 
 version := "0.1"
 
-scalaVersion := "2.13.1"
+scalaVersion := "2.11.12"
 
 libraryDependencies += "org.apache.kafka" %% "kafka" % "2.4.1"
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.2.0"
+libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10-assembly" % "2.2.0"
+
---
+++ src_project/Lab_Kafka/src/main/scala/Suparuku/SparkPOC.scala
@@ -0,0 +1,41 @@
+package Suparuku
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark._
+import org.apache.spark.streaming._
+import org.apache.log4j.Logger
+
+import org.apache.log4j.Level
+
+object SparkPOC extends App {
+  Logger.getLogger("org").setLevel(Level.OFF)
+  Logger.getLogger("akka").setLevel(Level.OFF)
+  val conf = new SparkConf().setAppName("FDP").setMaster("local[*]")
+  val streamingContext = new StreamingContext(conf, Seconds(1))
+
+  val kafkaParams = Map[String, Object] (
+    "bootstrap.servers" -> "localhost:9092",
+    "key.deserializer" -> classOf[StringDeserializer],
+    "value.deserializer" -> classOf[StringDeserializer],
+    "group.id" -> "i_must_be_unique_for_each_stream",
+    "auto.offset.reset" -> "earliest",
+    "enable.auto.commit" -> (false: java.lang.Boolean),
+    "key.separator" -> ","
+  )
+
+  val topics = Array("ALERT")
+  val stream = KafkaUtils.createDirectStream[String, String](
+    streamingContext,
+    PreferConsistent,
+    Subscribe[String, String](topics, kafkaParams)
+  )
+
+  stream.foreachRDD(rdd => rdd.map(record => record.value.split(',')).foreach(s => println("The key is "
+    + s.apply(0)
+    + " and the value is "
+    + s.apply(1))))
+  streamingContext.start()
+  streamingContext.awaitTermination()
+}
